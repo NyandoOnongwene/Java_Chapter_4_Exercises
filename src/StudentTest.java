f03d08c0263a029b7cfb2fdbbcfa3ddfcdda4ab2
NyandoOnongwene <nyandoonotex@gmail.com>
2024-04-08 01:39:55 +0100

    correct %n to /n and also removed unnessary semicolon

--- src/StudentTest.java
+++ src/StudentTest.java
@@ -4,8 +4,8 @@ public class StudentTest {
       Student account1 = new Student("Jane Green", 70);
       Student account2 = new Student("Nyando Onongwene",90);
 
-      System.out.printf("%s's letter grade is %s%n",
+      System.out.printf("%s's letter grade is %s/n",
               account1.getName(), account1.getLetterGrade());
-      System.out.printf("%s's letter grade is %s%n", account2.getName(), account2.getLetterGrade() );;
+      System.out.printf("%s's letter grade is %s%n", account2.getName(), account2.getLetterGrade() );
     }
 }
